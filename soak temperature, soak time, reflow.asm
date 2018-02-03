991ac56ddb8bb11d0364b6bf287c104efec3a093
valentine9510 <v.ssebuyungo@alumni.ubc.ca>
2018-02-03 14:29:38 -0800

    Add files via upload

--- soak temperature, soak time, reflow.asm
+++ soak temperature, soak time, reflow.asm
@@ -24,9 +24,6 @@ SendString:
 SendStringDone:
     ret
 
-dseg at 0x30
-toggle_value: ds 1
-
 cseg
 ; These 'equ' must match the wiring between the microcontroller and the LCD!
 LCD_RS equ P1.1
@@ -40,20 +37,24 @@ $NOLIST
 $include(LCD_4bit.inc) ; A library of LCD related functions and utility macros
 $LIST
 
-;                           1234567890123456    <- This helps determine the location of the counter
-soak_temp_text:  	db 'Soak   Temp    ', 0   ;'xxx Hxx Mxx Sxx'
-soak_time_text: 	db 'Soak   Time    ',0                
-reflow_temp_text:	db 'Reflow  Temp   ',0
-reflow_time_text:   db 'Reflow  Temp   ',0
-timer_message :     db 'Min   Sec  ',0
+;                       1234567890123456    <- This helps determine the location of the counter
+soak_temp_text:  	db ' Soak   Temp    ', 0   ;'xxx Hxx Mxx Sxx'
+soak_time_text: 	db ' Soak   Time    ',0                
+reflow_temp_text:	db ' Reflow  Temp   ',0
+reflow_time_text:   db ' Reflow  Time   ',0
+timer_message :     db 'Time in Secs:   ',0
 temp_message :      db 'Temperature:    ',0 
+empty :      		db '                ',0 
+time_message:       db 'Time in secs:   ',0 
+
 
 
 ;Buttons to select soak temperatur,soak time,reflow temperature,reflow time and enter button
-toggle_button 		    equ p2.4
-increment_button        equ p2.5
+toggle_button 			equ p2.4
+soak_time_button        equ p2.5
+reflow_temp_button    	equ p2.6
+reflow_time_button    	equ p2.7
 enter_button  			equ p4.5
-
 
 ;---------------------------------;
 ; Main program. Includes hardware ;
@@ -66,86 +67,113 @@ main:
  jmp start
 	; Initialization
 start:
-lcall toggle_function
-jmp start
+	;Set_Cursor(2,1)
+   	;Send_Constant_String(#empty)
+	jnb toggle_button,soak_temp_loop
+	jmp start
 
-;checks if toggle button has been pressed
-toggle_function:
-jnb toggle_button,toggle_function_inc
-ret
 
-;checks value of toggle_value to see whether it is at the last option
-toggle_function_inc:
-mov a, toggle_value
-cjne a,#0x4, next
-mov toggle_value,#0x0
-next:
-mov a, toggle_value
-add a, #0x1
-mov toggle_value, a
-sjmp reflow_temp_jmp
 
-;following lines all check if toggle is supposed to be on the given fcn and goes to the given loop
-reflow_temp_jmp:
-mov a, toggle_value
-cjne a, #0x1, reflow_time_jmp 
-ljmp reflow_temp_loop
 
-reflow_time_jmp:
-mov a, toggle_value
-cjne a, #0x2, soak_temp_jmp
-ljmp reflow_time_loop
 
-soak_temp_jmp:
-mov a, toggle_value
-cjne a, #0x3, soak_time_jmp
-ljmp soak_temp_loop
 
-soak_time_jmp:
-mov a, toggle_value
-cjne a, #0x4, toggle_function
-ljmp soak_time_loop
 	
-;loops for each option	
 soak_temp_loop:
-   Set_Cursor(1,1)
-   Send_Constant_String(#soak_temp_text)
-   jnb enter_button,start
-   Set_Cursor(2,1)
-   Send_Constant_String(#temp_message)
+	Set_Cursor(1,1)
+   	Send_Constant_String(#soak_temp_text)
+   	Wait_Milli_Seconds(#200) 
+   	jb toggle_button,$
+	jnb enter_button,soak_temp_loop_2
+	ljmp soak_time_loop
+
+soak_temp_loop_2:	
+   	Set_Cursor(1,1)
+   	Send_Constant_String(#soak_temp_text)
+   	Set_Cursor(2,1)
+   	Send_Constant_String(#temp_message)
    
-   jnb enter_button,$
-   ljmp start
+	Wait_Milli_Seconds(#200)   
+   	jb enter_button,$
+   	Set_Cursor(2,1)
+   	Send_Constant_String(#empty)
+   	Wait_Milli_Seconds(#200) 
+   	ljmp start
    
 soak_time_loop:
-   Set_Cursor(1,1)
-   Send_Constant_String(#soak_time_text)
-   jnb enter_button,start_1
-   Set_Cursor(2,1)
-   Send_Constant_String(#timer_message)
-   jnb enter_button,$
-   ljmp start_1 
+	Set_Cursor(1,1)
+   	Send_Constant_String(#soak_time_text)
+   	Wait_Milli_Seconds(#200) 
+   	jb toggle_button,$
+	jnb enter_button,soak_time_loop_2
+	ljmp reflow_time_loop
+	
+	
+soak_time_loop_2:	
+   	Set_Cursor(1,1)
+   	Send_Constant_String(#soak_time_text)
+   	Set_Cursor(2,1)
+   	Send_Constant_String(#time_message)
+   	
+   	
+   	Wait_Milli_Seconds(#200)   
+   	jb enter_button,$
+   	Set_Cursor(2,1)
+   	Send_Constant_String(#empty)
+   	Wait_Milli_Seconds(#200) 
+   	ljmp start
    
-start_1:
-ljmp start
-
 reflow_time_loop:
-   Set_Cursor(1,1)
-   Send_Constant_String(#reflow_time_text)
-   jnb enter_button,start_1
-   Set_Cursor(2,1)
-   Send_Constant_String(#timer_message)
-   jnb enter_button,$
-   ljmp start_1    
+	Set_Cursor(1,1)
+   	Send_Constant_String(#reflow_time_text)
+   	Wait_Milli_Seconds(#200)
+   	jb toggle_button,$
+	jnb enter_button,reflow_time_loop_2
+	ljmp reflow_temp_loop
+	
+	
+	
+reflow_time_loop_2:	
+   	Set_Cursor(1,1)
+   	Send_Constant_String(#reflow_time_text)
+   	Set_Cursor(2,1)
+   	Send_Constant_String(#time_message)
+   	
+   	
+   	
+   	Wait_Milli_Seconds(#200)   
+   	jb enter_button,$
+   	Set_Cursor(2,1)
+   	Send_Constant_String(#empty)
+   	Wait_Milli_Seconds(#200) 
+   	ljmp start
+      
 	
 	
 reflow_temp_loop:
-   Set_Cursor(1,1)
-   Send_Constant_String(#soak_time_text)
-   jnb enter_button,start_1
-   Set_Cursor(2,1)
-   Send_Constant_String(#timer_message)
-   jnb enter_button,$
-   ljmp start_1 	
+	Set_Cursor(1,1)
+   	Send_Constant_String(#reflow_temp_text)
+   	Wait_Milli_Seconds(#200) 
+   	jb toggle_button,$
+	jnb enter_button,reflow_temp_loop_2
+	ljmp start
+	
+	
+	
+	
+reflow_temp_loop_2:	
+	Set_Cursor(1,1)
+	Send_Constant_String(#reflow_temp_text)
+	Set_Cursor(2,1)
+	Send_Constant_String(#temp_message)
+	
+	
+	
+	Wait_Milli_Seconds(#200)   
+   	jb enter_button,$
+   	Set_Cursor(2,1)
+   	Send_Constant_String(#empty)
+   	Wait_Milli_Seconds(#200) 
+   	ljmp start
+    	
 	
 END
