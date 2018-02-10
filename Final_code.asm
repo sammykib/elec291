2c90a16ec2b62c8c54588aa0f509d1265b54f54d
nickhowe <32752545+nickhowe@users.noreply.github.com>
2018-02-10 15:46:31 -0800

    Add files via upload

--- Final_code.asm
+++ Final_code.asm
@@ -36,7 +36,10 @@ TIMER2_RELOAD  	EQU ((65536-(CLK/TIMER2_RATE)))
 
 DSEG at 30H
 
-Temperature:	ds 5 ;temperature BCD value
+Temp:			ds 5 ;temperature BCD value
+bcd: 			ds 5
+Thermo:			ds 2
+amb: 			ds 5
 Count1ms: 		ds 2 ; used to count for one second
 
 Secs_BCD:		ds 5 ;These two values are for the displayed runtime
@@ -50,10 +53,13 @@ BCD_reflow_time: 	ds 2
 SoakTime_Secs:   	ds 2
 ReflowTime_Secs: 	ds 2
 
+
+
 pwm_count:	 	ds 1
 pwm:		 	ds 1
-temp_cool:			ds 1
-state:				ds 1
+temp_cool:		ds 1
+state:			ds 1
+settings_num:	ds 1
 
 ;arithmetic variables
 x: 			ds 4
@@ -67,23 +73,15 @@ Result: 		ds 2
 BSEG
 
 ;State Flags - Only one flag on at once 
-PreheatState_Flag:		dbit 1
 SoakState_Flag: 		dbit 1
 RampState_Flag:	 		dbit 1
 ReflowState_Flag: 		dbit 1
 CooldownState_Flag: 	dbit 1
 
-soak_menu_flag: 		dbit 1
-reflow_menu_flag:		dbit 1
-
-;Transition Flag turns on when state is changing, and turns off shortly afterwards
-;Use with State flags in logic in order to determine what to do eg. beeps to play when x state is (just recently) on and transition flag is on as well
-;Transition_Flag: 		dbit 1 
 
 CoolEnoughToOpen_Flag: 		dbit 1
 CoolEnoughToTouch_Flag: 	dbit 1
 Cooldowntouch_Flag: 		dbit 1
-;DoorOpen_Flag: 			dbit 1
 
 mf: 				dbit 1 ;Math Flag for use with math32.inc
 
@@ -102,6 +100,11 @@ CE_ADC  	EQU P2.0
 MY_MOSI 	EQU P2.1
 MY_MISO 	EQU P2.2
 MY_SCLK 	EQU P2.3
+
+FT93C66_CE   EQU P2.4 ;might need to Pick a different pin
+FT93C66_MOSI EQU P2.1 
+FT93C66_MISO EQU P2.2
+FT93C66_SCLK EQU P2.3 
 
 ;LCD pins
 LCD_RS  	EQU P1.2
@@ -133,29 +136,10 @@ $LIST
 $NOLIST
 $include(math32.inc) ; for math functions
 $LIST
+$NOLIST
+$include(FT93C66.inc); for non-volitile memory chip 93C66 commands
+$LIST
 
-;list of preset strings for the menu
-soak_temp_text:     db ' Soak   Temp    ',0   
-soak_time_text:     db ' Soak   Time    ',0                
-reflow_temp_text:   db ' Reflow  Temp   ',0
-reflow_time_text:   db ' Reflow  Time   ',0
-timer_message :     db 'Time in Secs:   ',0
-temp_message :      db 'Temperature:    ',0 
-empty :      		db '                ',0 
-time_message:       db 'Time in secs:   ',0 
-
-;menu messages
-running_menu_1:     db 'State:xxxxxxx',0
-running_menu_2:     db 'xxm xxs',0
-main_menu_1:        db 'Soldering Profile:',0
-main_menu_2:        db '[1]New [2]Load',0
-
-;text to display which state we're in
-soak_state:         db 'Soak',0
-to_peak:            db 'To Peak',0
-reflow_state:       db 'Reflow',0
-cool_state:         db 'Cooling',0
-done_state:         db 'Done',0
 
 ;-------------------------------------------;
 ;            SPI Initialization             ;
@@ -237,10 +221,10 @@ Timer0_Init:
 Timer0_ISR:
 	;clr TF0  ; According to the data sheet this is done for us already.
 	; In mode 1 we need to reload the timer.
-	clr TR0
+	;clr TR0
 	mov TH0, #high(TIMER0_RELOAD)
 	mov TL0, #low(TIMER0_RELOAD)
-	setb TR0
+	;setb TR0
 	cpl SOUND_OUT
 	reti
 ;-------------------------------------------;
@@ -342,13 +326,30 @@ ContinueISR:
 	da a
 	mov Secs_BCD, a
 	cjne a, Mins_BCD, ContinueISR_1
-	Preheat_Abort(Temperature+1, Temperature+2)
+	;Preheat_Abort(Temperature+1, Temperature+2)
 ContinueISR_1:	
 	clr a
 	mov a, Mins_BCD
 	add a,#0x01
 	da a
 	mov Mins_BCD,a
+SendThingsToPuttyandMath:
+; Get Thermocouple data
+    clr CE_ADC
+    mov R0, #00000001B; Start bit:1
+    lcall DO_SPI_G
+    mov R0, #10010000B ; Single ended, read channel 1
+    lcall DO_SPI_G
+    mov a, R1          ; R1 contains bits 8 and 9
+    anl a, #00000011B  ; We need only the two least significant bits
+    mov Thermo+1, a    ; Save result high.
+    mov R0, #55H ; It doesn't matter what we transmit...
+    lcall DO_SPI_G
+    mov Thermo, R1     
+; R1 contains bits 0 to 7.  Save result low.
+    setb CE_ADC
+;    lcall Delay
+    lcall Do_Something_With_Result2
 Timer2_ISR_done:
 	pop psw
 	pop acc
@@ -363,6 +364,7 @@ Timer2_ISR_done:
     	;Initialize Serial Port Interface, and LCD
     	lcall InitSerialPort
     	lcall INIT_SPI
+    	lcall FT93C66_INIT_SPI
     	lcall LCD_4BIT
     	lcall Timer2_Init
     	lcall Timer0_Init
@@ -373,27 +375,26 @@ Timer2_ISR_done:
     	clr RampState_Flag
     	clr ReflowState_Flag
     	clr CooldownState_Flag
-    	setb PreheatState_Flag ;Set Preheat flag to 1 at power on (it won't start preheating until it gets to that loop via Start button)
+    	;setb PreheatState_Flag ;Set Preheat flag to 1 at power on (it won't start preheating until it gets to that loop via Start button)
    
     	clr mf
-    	clr CoolEnoughToOpen_Flag
-		clr CoolEnoughToTouch_Flag
-		clr soak_menu_flag
-		clr reflow_menu_flag
 		clr POWER
+		clr TR0
 		mov pwm, #0x00
-		clr Length_Flag
+		mov a,#0x00
+		mov settings_num,a
     
     	;Set Presets
-    	mov BCD_soak_temp, 	#0x40
-		mov BCD_soak_temp+1, 	#0x01
-		mov BCD_reflow_temp, 	#0x19
-		mov BCD_reflow_temp+1,	#0x02
+    	mov BCD_soak_temp, 	#0xA0
+		;mov BCD_soak_temp+1, 	#0x01
+		mov BCD_reflow_temp, 	#0xE1
+		;mov BCD_reflow_temp+1,	#0x02
 	
-		mov BCD_soak_time, 	#0x00
-		mov BCD_soak_time+1,	#0x01
-		mov BCD_reflow_time, 	#0x30
-		mov BCD_reflow_time+1,	#0x00
+		mov BCD_soak_time, 	#0x3C
+		;mov BCD_soak_time+1,	#0x01
+		mov BCD_reflow_time, 	#0x1E
+		;mov BCD_reflow_time+1,	#0x00
+		lcall Save_values
 	
 		mov Mins_BCD, 		#0x00
 		mov Secs_BCD, 		#0x00 
@@ -403,16 +404,22 @@ Timer2_ISR_done:
 	mov SoakTime_Secs, 	#0x00
 	
 	;Give temp an initial value so it doesn't auto-abort because of an unknown
-	mov Temperature+0, 	#0x00
-	mov Temperature+1, 	#0x00
-	mov Temperature+2, 	#0x00
+	mov Temp+0, 	#0x00
+	mov Temp+1, 	#0x00
+	mov Temp+2, 	#0x00
 	mov temp_cool, #0x60
  	sjmp forever
  
 forever:
 ;get SPI code from Jackey
 
-lcall start_menu
+jb start_menu_button,next
+jnb start_menu_button,next
+jb start_menu_button,$
+;lcall menu branch
+
+
+next:
 lcall Reflow_States
 
 ljmp forever
@@ -425,7 +432,10 @@ state_start:
 cjne a, #0, state_tosoak
 mov pwm, #0
 jb Button_1, state_start_done
-jnb Button_1, $ ; Wait for key release
+;jnb Button_1, $ ; Wait for key release
+setb TR0
+Wait_Milli_Seconds(#50)
+clr TR0
 mov state, #1
 state_start_done:
 ret
@@ -436,22 +446,26 @@ mov pwm, #100
 ;mov sec, #0
 mov a, BCD_soak_temp
 clr c
-subb a, Temperature
+subb a, Temp
 jnc state_tosoak_done
 mov state, #2
+setb TR0
+Wait_Milli_Seconds(#50)
+clr TR0
 setb SoakState_Flag; start timer for soaking
 state_tosoak_done:
 ret
 
 state_soak:
 cjne a, #2, state_topeak
-Set_Cursor(1,7)
-Send_Constant_String(#soak_state)
 mov pwm, #20
 mov a, BCD_soak_time
 clr c
 subb a, SoakTime_Secs
 jnc state_soak_done
+setb TR0
+Wait_Milli_Seconds(#100)
+clr TR0
 mov state, #3
 clr SoakState_Flag
 state_soak_done:
@@ -459,13 +473,14 @@ ret
 
 state_topeak:
 cjne a, #3, state_reflow
-Set_Cursor(1,7)
-Send_Constant_String(#to_peak)
 mov pwm, #100
 mov a, BCD_reflow_temp
 clr c
-subb a, Temperature
+subb a, Temp
 jnc state_topeak_done
+setb TR0
+Wait_Milli_Seconds(#100)
+clr TR0
 mov state, #3
 setb ReflowState_Flag
 state_topeak_done:
@@ -473,13 +488,14 @@ ret
 
 state_reflow:
 cjne a, #4, state_cool
-Set_Cursor(1,7)
-Send_Constant_String(#reflow_state)
 mov pwm, #20
 mov a, BCD_reflow_time
 clr c
 subb a, ReflowTime_Secs
 jnc state_reflow_done
+setb TR0
+Wait_Milli_Seconds(#100)
+clr TR0
 mov state, #5
 clr ReflowState_Flag
 state_reflow_done:
@@ -487,13 +503,15 @@ ret
 
 state_cool:
 cjne a, #5, state_done
-Set_Cursor(1,7)
-Send_Constant_String(#cool_state)
 mov pwm, #0
 mov a, temp_cool
 clr c
-subb a, Temperature
+subb a, Temp
 jnc state_cool_done
+setb TR0
+Wait_Milli_Seconds(#200)
+Wait_Milli_Seconds(#200)
+clr TR0
 mov state, #6
 state_cool_done:
 ret
@@ -501,32 +519,315 @@ ret
 state_done:
 ;compliment timer 2 which speaker will be used in for "end beep" output
 ; Display 'OPEN DOOR' on LCD
-Set_Cursor(1,7)
-Send_Constant_String(#done_state)
 mov a, #40H
 clr c
-subb a, Temperature
+subb a, Temp
 jnc state_done_done
+setb TR0
+Wait_Milli_Seconds(#50)
+clr TR0
+setb TR0
+Wait_Milli_Seconds(#50)
+clr TR0
+setb TR0
+Wait_Milli_Seconds(#50)
+clr TR0
+setb TR0
+Wait_Milli_Seconds(#50)
+clr TR0
+setb TR0
+Wait_Milli_Seconds(#50)
+clr TR0
+setb TR0
+Wait_Milli_Seconds(#50)
+clr TR0
+mov state, #0
 ;Display "SAFE TEMP"
 state_done_done:
 ret
 
-;code for the main menu and running menu
-running_menu: 
-Set_Cursor(1,1)
-Send_Constant_String(#main_menu_1)
-Set_Cursor(2,1)
-Send_Constant_String(#main_menu_2)
-ret
+Save_values:
+    lcall FT93C66_Write_Enable
+    mov a,settings_num
+    CJNE a,#0x0,settingone
+    mov dptr,#0x0 
+    jmp store
+    settingone:
+    CJNE a,#0x1,settingtwo
+    mov dptr,#0x4 
+    jmp store
+    settingtwo:
+    CJNE a,#0x2,settingthree
+    mov dptr,#0x8 
+    jmp store
+    settingthree:
+    CJNE a,#0x3,returnfromsetting
+    mov dptr,#0x12 
+    
+    store:
+    mov a,BCD_soak_time  ; Value to write at location
+    lcall FT93C66_Write
+    inc dptr
+    
+    mov a,BCD_soak_temp  ; Value to write at location
+    lcall FT93C66_Write
+    inc dptr
+    
+    mov a,BCD_reflow_time  ; Value to write at location
+    lcall FT93C66_Write
+    inc dptr
+    
+    mov a,BCD_reflow_time  ; Value to write at location
+    lcall FT93C66_Write
+    returnfromsetting:
+    ret
+  
+Read_values:
+    lcall FT93C66_Write_Enable
+    mov a,settings_num
+    CJNE a,#0x0,settingone2
+    mov dptr,#0x0 
+    jmp reading
+    settingone2:
+    CJNE a,#0x1,settingtwo2
+    mov dptr,#0x4 
+    jmp reading
+    settingtwo2:
+    CJNE a,#0x2,settingthree2
+    mov dptr,#0x8 
+    jmp reading
+    settingthree2:
+    CJNE a,#0x3,returnfromsetting
+    mov dptr,#0x12 
+    
+	reading:
+    lcall FT93C66_Read
+    mov BCD_soak_time,a  ; Value to write at location
+   
+    inc dptr
+    lcall FT93C66_Read
+    mov BCD_soak_temp,a  ; Value to write at location
+   
+    inc dptr
+    lcall FT93C66_Read
+    mov BCD_reflow_time,a  ; Value to write at location
+   
+    inc dptr
+    lcall FT93C66_Read
+    mov BCD_reflow_time,a  ; Value to write at location
+    ret
+    
+    Do_Something_With_Result2:
+	Load_x(0)
+	Load_y(0)
+    mov x+0, Thermo+0
+    mov x+1, Thermo+1
 
-main_menu:
-Set_Cursor(1,1)
-Send_Constant_String(#main_menu_1)
-Set_Cursor(2,1)
-Send_Constant_String(#main_menu_2)
-;button instructs for going to new/load screens
-ret
+	Load_y(47)
+    lcall mul32
+	Load_y(2400)
+	lcall add32
+	lcall hex2Temp
+    mov DPTR, #space
+    lcall SendString ;CODE FOR DEBUGGING ONLY
+	Send_BCD(Temp+2)
+    Send_BCD(Temp+1)
+    Send_BCD(Temp+0)
+    ret
 
- 
+hex2Temp:
+	push acc
+	push psw
+	push AR0
+	push AR1
+	push AR2
+	
+	clr a
+	mov Temp+0, a ; Initialize BCD to 00-00-00-00-00 
+	mov Temp+1, a
+	mov Temp+2, a
+	mov Temp+3, a
+	mov Temp+4, a
+	mov r2, #32  ; Loop counter.
+
+hex2Temp_L0:
+	; Shift binary left	
+	mov a, x+3
+	mov c, acc.7 ; This way x remains unchanged!
+	mov r1, #4
+	mov r0, #(x+0)
+hex2Temp_L1:
+	mov a, @r0
+	rlc a
+	mov @r0, a
+	inc r0
+	djnz r1, hex2Temp_L1
+    
+	; Perform bcd + bcd + carry using BCD arithmetic
+	mov r1, #5
+	mov r0, #(Temp+0)
+hex2Temp_L2:   
+	mov a, @r0
+	addc a, @r0
+	da a
+	mov @r0, a
+	inc r0
+	djnz r1, hex2Temp_L2
+
+	djnz r2, hex2Temp_L0
+
+	pop AR2
+	pop AR1
+	pop AR0
+	pop psw
+	pop acc
+	ret
+Do_Something_With_Result:
+    Set_Cursor(1,4)
+	Load_x(0)
+	Load_y(0)
+	mov x+0, Result
+	mov x+1, Result+1
+	Load_y(4096)
+	lcall mul32
+	Load_y(1023)
+	lcall div32
+	Load_y(2730)
+	lcall sub32
+	lcall hex2bcd
+	Send_BCD(bcd+2)
+	Send_BCD(bcd+1)
+	Send_BCD(bcd+0)
+    ret
+max_time:
+	mov a,time_counter
+	cjne a, #0x20, return_max_time
+	mov a,#0x0
+	mov hundreds_value,a
+	mov time_counter,a
+	lcall flicker_message
+	return_max_time:
+	ret
+
+	flicker_message:
+	Set_Cursor(1,1)
+	Send_Constant_String(#Max_message_1)
+	Set_Cursor(2,1)
+	Send_Constant_String(#Max_message_2)
+	Wait_Milli_Seconds(#250)
+	Wait_Milli_Seconds(#250)
+	Wait_Milli_Seconds(#250)
+	Wait_Milli_Seconds(#250)
+	Set_Cursor(2,1)
+	Send_Constant_String(#Max_message_3)
+	Wait_Milli_Seconds(#250)
+	Wait_Milli_Seconds(#250)
+	Wait_Milli_Seconds(#250)
+	Wait_Milli_Seconds(#250)
+	jb increment_button,go_flicker  
+	ret
+go_flicker:
+	ljmp flicker_message
+
+max_temp:
+	mov a,temp_counter
+	cjne a, #0x50, return_max_temp
+	mov a,#0x0
+	mov hundreds_value,a
+	mov temp_counter,a
+	lcall flicker_message
+	return_max_temp:
+	ret
+
+
+
+save:
+	Load_x(hundreds_value)
+	Load_y(100)
+	lcall mul32
+	Load_y(save_value)
+	lcall add32
+	mov save_value,x
+	ret
+	
+
+increment_temp:
+	jb increment_button,$
+	Wait_Milli_Seconds(#40)
+	mov a,hundreds_value
+	cjne a, #0x2,temp_not_50
+	lcall max_temp
+
+
+temp_not_50:
+	mov a,temp_counter
+	cjne a, #0x99, increment_temp_next
+	mov temp_counter, #0x0
+	mov a,hundreds_value                  ;mov hundreds to a
+	cjne a, #0x3, increment_100       ;check if it has reached 400C
+	mov hundreds_value,#0x0 		          ;reset to zero
+	ljmp increment_temp_next
+
+increment_100: 
+	add a,#0x1
+	mov hundreds_value,a                   	  ;increment 100 when accumulator reaches 99
+	mov a,#0x0
+	mov temp_counter, a
+	mov display_value,temp_counter 
+	ret
+	
+increment_temp_next:
+	mov a,temp_counter
+	add a,#0x1
+	da a
+	mov temp_counter, a
+	mov display_value,temp_counter
+	ret 
+
+increment_time:
+	jb increment_button,$
+	Wait_Milli_Seconds(#40)
+	mov a,hundreds_value
+	cjne a, #0x1,time_not_20
+	lcall max_time
+	
+time_not_20:
+	mov a,time_counter
+	cjne a, #0x99, increment_time_next
+	mov time_counter, #0x0 		          ;reset to zero
+	mov a,hundreds_value                  ;mov hundreds to a
+	cjne a, #0x2, increment_100_time       ;check if it has reached 400C
+	mov hundreds_value,#0x0 		          ;reset to zero
+	ljmp increment_temp_next 
+	
+increment_100_time: 
+	add a,#0x1
+	mov hundreds_value,a                   	  ;increment 100 when accumulator reaches 99
+mov a,#0x0
+mov time_counter, a
+mov display_value,time_counter 
+ret
+	
+increment_time_next:
+mov a,time_counter
+add a,#0x1
+da a
+mov time_counter, a
+mov display_value,time_counter
+ret 
+
+;code to save the values
+;save:
+;ret
+;---------------------------------;
+; Main program. Includes hardware ;
+; initialization and 'forever'    ;
+; loop.                           ;
+;---------------------------------;
+Delay:
+    Wait_Milli_Seconds(#100)
+    Wait_Milli_Seconds(#100)
+    ret
+
 END
   
